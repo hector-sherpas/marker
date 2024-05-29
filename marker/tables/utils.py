c0493c7c8148f0f4c69f9d4844fe1b01c9653084
Héctor Tundidor <hectortundidor@Hectors-MacBook-Air.local>
2024-05-29 11:19:09 +0200

    Sorting table blocks when there are elements at different heights

--- marker/tables/utils.py
+++ marker/tables/utils.py
@@ -3,23 +3,61 @@ from sklearn.cluster import DBSCAN
 import  numpy as np
 
 
-def sort_table_blocks(blocks):
-    
-    y_coords = [block.bbox[1] if hasattr(block, "bbox") 
-                else block["bbox"][1] 
-                for block in blocks]
+def get_clusters(coords):
+    coords = np.array(coords).reshape(-1, 1)
 
-    y_coords = np.array(y_coords).reshape(-1, 1)
-
-    clustering = DBSCAN(eps=3, min_samples=1).fit(y_coords)
+    clustering = DBSCAN(eps=3, min_samples=1).fit(coords)
 
     clusters = clustering.labels_
+
+    return clusters
+
+
+def get_y_coords_from_blocks(blocks, criteria="y_min"):
+
+    if criteria=="y_min":
+        y_coords = [block.bbox[1] if hasattr(block, "bbox") 
+                    else block["bbox"][1] 
+                    for block in blocks]
     
+    elif criteria=="y_middle":
+        y_coords = [block.bbox[1] + (block.bbox[3] - block.bbox[1])/2 if hasattr(block, "bbox") 
+                    else block["bbox"][1] + (block["bbox"][3] - block["bbox"][1])/2 
+                    for block in blocks]
+    
+    elif criteria=="y_max":
+        y_coords = [block.bbox[3] if hasattr(block, "bbox") 
+                    else block["bbox"][3] 
+                    for block in blocks]
+        
+    return y_coords
+
+
+def group_blocks(blocks, criteria):
+    
+    if isinstance(blocks, list):
+        y_coords = get_y_coords_from_blocks(blocks, criteria=criteria)
+    elif isinstance(blocks, dict):
+        y_coords = [np.mean(get_y_coords_from_blocks(grouped_blocks, criteria)) for grouped_blocks in blocks.values()]
+        blocks = blocks.values()
+
+    clusters= get_clusters(y_coords)
+
     vertical_groups = {}
     for group, block, _ in sorted(zip(clusters.tolist(), blocks, y_coords), key=lambda x: x[2]):
         if group not in vertical_groups:
             vertical_groups[group] = []
-        vertical_groups[group].append(block)
+        vertical_groups[group].extend([block] if isinstance(block, dict) 
+                                      else block)
+
+    return 
+
+
+def sort_table_blocks(blocks):
+
+    vertical_groups = group_blocks(blocks, criteria="y_max")
+    vertical_groups = group_blocks(vertical_groups, criteria="y_middle")
+    vertical_groups = group_blocks(vertical_groups, criteria="y_min")
 
     # Sort each group horizontally and flatten the groups into a single list
     sorted_blocks = []
